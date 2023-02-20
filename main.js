768788632ea408e319e7aac0915525851e683fc1
Alexander_Leon <111066978+006Leon600@users.noreply.github.com>
2023-02-20 13:49:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const funciones=require('./Modulos/funciones');
 console.table(os.frecuencia.slice(0,1).map((registro)=> registro.times));
 console.log(" * Sis Op: "+os.SO);
 console.log(" * Ram Disp",os.rdisp,"Byte");
-console.log(" * Ram en GB",funciones.disporam(os.rdisp),"GB");
+console.log(" * Ram en GB",funciones.dispor(os.rdisp),"GB");
 console.log(" * Ram Tot",os.rtot,"Byte");
-console.log(" * Ram Tot en GB",funciones.totalram(os.rtot),"GB");
+console.log(" * Ram Tot en GB",funciones.totalr(os.rtot),"GB");
