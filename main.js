e53b229b3bdcf786f0da03171fad6148d11d5b79
Alexander_Leon <111066978+006Leon600@users.noreply.github.com>
2023-02-20 13:46:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const os=require('./Modulos/os');
 const funciones=require('./Modulos/funciones');
 console.table(os.frecuencia.slice(0,1).map((registro)=> registro.times));
-console.log("Sistema Operativo: "+os.SO);
-console.log("Ram Disponible",os.ramdisp,"Byte");
-console.log("Ram en GB",funciones.disporam(os.ramdisp),"GB");
-console.log("Ram Total",os.ramtot,"Byte");
-console.log("Ram Total en GB",funciones.totalram(os.ramtot),"GB");
+console.log(" * Sis Op: "+os.SO);
+console.log(" * Ram Disp",os.rdisp,"Byte");
+console.log(" * Ram en GB",funciones.disporam(os.rdisp),"GB");
+console.log(" * Ram Tot",os.rtot,"Byte");
+console.log(" * Ram Tot en GB",funciones.totalram(os.rtot),"GB");
